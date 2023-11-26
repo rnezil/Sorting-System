443447ae3a8b5edee7a738e92a8d3ef4331838c0
rnezil <rylandnezil@gmail.com>
2023-11-26 15:03:38 -0800

    Fixes bug where tail pointer would be invalid upon dequeueing from size 1 list

--- LinkedQueue.h
+++ LinkedQueue.h
@@ -14,7 +14,7 @@ void	destroyLink	(link **oldLink);
 void 	setup		(link **h, link **t);
 void 	clearQueue	(link **h, link **t);
 void 	enqueue		(link **h, link **t, link **nL);
-void 	dequeue		(link **h, link **deQueuedLink);
+void 	dequeue		(link **h, link **t, link **deQueuedLink);
 char	firstValue	(link **h);
 char 	isEmpty		(link **h);
 int 	size		(link **h, link **t);
